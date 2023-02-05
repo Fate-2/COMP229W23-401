db45a91668847421f21b8dd1d1dc2d2f93342879
Shemal Grey <shemalgrey@gmail.com>
2023-02-05 17:14:26 -0500

    COMP229W23S401 - project1

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
-const http = require('http');
+const connect = require('connect');
+const app = connect();
 
-http.createServer(   
-    (req, res)  => {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>Hello World</h1>');
-    }
-    
-    
-    
-).listen(3000);
+// logger middleware
+function logger(req, res, next){
+    console.log(req.method, req.url);
+    next();
+}
 
+function goodbyeWorld(res, res, next){
+    res.setHeader('Content-Type','text/plain');
+res.end('Goodbye World');
+}
 
+function helloWorld(req, res, next){
+res.setHeader('Content-Type','text/plain');
+res.end('Hello World');
+}
+
+app.use(logger);
+app.use('/hello', helloWorld);
+app.use('/goodbye', goodbyeWorld);
+
+app.listen(3000);
 
 console.log('Server running at http://localhost:3000');
