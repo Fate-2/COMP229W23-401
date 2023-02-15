b9c448970a1ab2b3d5125e070e6698aecfdbff3c
Shemal Grey <shemalgrey@gmail.com>
2023-02-15 17:33:16 -0500

    git push

--- app/controllers/movies.js
+++ app/controllers/movies.js
@@ -1,30 +1,37 @@
 import movieModel from '../models/movies.js';
 
+import { UserDisplayName } from "../utils/index.js";
 
-//R ead
+//R ead Operations
 export function DisplayMoviesList(req, res, next){
-    movieModel.find(function(error, movieCollection){
+    movieModel.find(function (error, movieCollection){
         if(error){
             console.error(error);
             res.end(error);
         }
 
-        res.render('index', {title: 'Movie List', page: 'movies/list', movies: movieCollection})
+        console.log(movieCollection);
+
+        res.render('index', {title: 'Movie List', 
+            page: 'movies/list', 
+            movies: movieCollection,
+            displayName: UserDisplayName(req)  })
     })
 }
 
 
 //C reate
-export function DisplayMoviesPage(req, res, next){
-    res.render('index', {title: 'Add Movie', page: 'movies/edit', movie: {}})
+export function DisplayMoviesAddPage(req, res, next){
+    res.render('index', {title: 'Add Movie', page: 'movies/edit', movie: {},
+    displayName: UserDisplayName(req)})
 }
 
 export function ProcessMoviesAddPage(req, res, next){
     let newMovie = movieModel({
         name: req.body.name,
-        year: req.body.year,
+        year: req.body.year,       
         director: req.body.director,
-        genre: req.body.genre,
+        genre: req.body.genre,   
         runtime: req.body.runtime
     });
 
@@ -33,36 +40,37 @@ export function ProcessMoviesAddPage(req, res, next){
             console.error(error);
             res.end(error);
         }
-        
+
         res.redirect('/movie-list');
     })
 }
 
 //U pdate
-export function DisplayMoviesPage(req, res, next){
+export function DisplayMoviesEditPage(req, res, next){
 
-let id = req.params.id;
-
-movieModel.findById(id, function(error, movie){
-    if(error){
-        console.error(error);
-        res.end(error);
-    }
-
-    res.render('index', {title: 'Edit Movie', page: 'movies/edit', movie})
-})
-
-}
-export function ProcessMoviesEditPage(req, res, next){
     let id = req.params.id;
+
+    movieModel.findById(id, function(error, movie){
+        if(error){
+            console.error(error);
+            res.end(error);
+        }
+
+        res.render('index', {title: 'Edit Movie', page: 'movies/edit', movie,
+        displayName: UserDisplayName(req)})
+    })    
+}
+
+export function ProcessMoviesEditPage(req, res, next){
+    let id = req.params.id
 
 
     let editMovie = movieModel({
         _id: req.body.id,
         name: req.body.name,
-        year: req.body.year,
+        year: req.body.year,       
         director: req.body.director,
-        genre: req.body.genre,
+        genre: req.body.genre,   
         runtime: req.body.runtime
     });
 
@@ -71,10 +79,12 @@ export function ProcessMoviesEditPage(req, res, next){
             console.error(error);
             res.end(error);
         }
-        
+
         res.redirect('/movie-list');
     })
 }
+
+
 //D elete
 export function ProcessMoviesDelete(req, res, next){
     let id = req.params.id
@@ -84,8 +94,7 @@ export function ProcessMoviesDelete(req, res, next){
             console.error(error);
             res.end(error);
         }
-        
-        res.redirect('/movie-list');
 
+        res.redirect('/movie-list');
     })
 }
