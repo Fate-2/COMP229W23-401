db38764aa0a180dbbf7867899ce63ecad63bbae7
Shemal Grey <shemalgrey@gmail.com>
2023-02-15 18:10:42 -0500

    Removed node_modules, connected to mongo atlas

--- config/index.js
+++ config/index.js
@@ -1,2 +1,4 @@
 export const Secret = "MySecret";
-export const MongoURI = "mongodb://127.0.0.1/media"
+//export const MongoURI = "mongodb://127.0.0.1/media"
+export const MongoURI = "mongodb+srv://shemal:<Monkamanbev>@cluster0.irkqw6d.mongodb.net/media?retryWrites=true&w=majority"
+
