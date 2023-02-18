9bede2737efb267e02b58ed4412c37aaa346f420
Shemal Grey <shemalgrey@gmail.com>
2023-02-18 12:56:19 -0500

    Fixed name errors

--- server.js
+++ server.js
@@ -3,23 +3,21 @@ debug('comp-229');
 
 import http from 'http';
 
-// import the app
+// import the app 
 import app from './app/app.js';
 
-const PORT = normilizePort(process.env.PORT || 3000);
+const PORT = normalizePort(process.env.PORT || 3000);
 app.set('port', PORT);
-
 
 const server = http.createServer(app);
 
 server.listen(PORT);
 server.on('error', onError);
-server.on('listening', onListening)
+server.on('listening', onListening);
 
 
-
-// HELPER FUCNTIONS
-function normilizePort(val){
+// HELPER FUNCTIONS
+function normalizePort(val){
     let port = parseInt(val, 10);
 
     if(isNaN(port)){
@@ -38,21 +36,21 @@ function onError(){
         throw error;
     }
 
-    let bind = typeof port == 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+    let bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
 
     switch (error.code){
         case 'EACCESS':
-            console.error(bind + 'requires elevated privileges');
+            console.error(bind + ' requires elevated privileges');
             process.exit(1);
             break;
-            case 'EADDINUSE':
-                console.error(bind + 'is already in use');
-                process.exit(1);
-                break; 
-            default:
-                throw error;
+        case 'EADDINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
     }
 }
 
@@ -60,5 +58,5 @@ function onListening(){
     let addr = server.address();
     let bind = 'pipe ' + addr;
     debug('Listening on ' + bind);
-    console.log('Listening on ', addr);
+    console.log('Application Started Successfully');
 }
