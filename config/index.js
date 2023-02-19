f16246829d404daf5c5bc606d4220a8d5759b95e
Shemal Grey <shemalgrey@gmail.com>
2023-02-19 11:23:47 -0500

    fixed mongo pass

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 export const Secret = "MySecret";
 //export const MongoURI = "mongodb://127.0.0.1/media"
-export const MongoURI = "mongodb+srv://shemal:<Monkamanbev>@cluster0.irkqw6d.mongodb.net/media?retryWrites=true&w=majority"
+export const MongoURI = "mongodb+srv://shemal:straightbev123@cluster0.irkqw6d.mongodb.net/media?retryWrites=true&w=majority"
 
